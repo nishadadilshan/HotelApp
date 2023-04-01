965124a39a07271cb0ea738d4ae3f45371b50e54
SYNAPSYS\dilshanw <dilshan.weerathunga@sysnapsys.lk>
2023-04-02 01:46:35 +0530

    New App added app bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Screens/Home';
 
 function App() {
   return (
     <div className="App">
-
+      <Home />
     </div>
   );
 }
